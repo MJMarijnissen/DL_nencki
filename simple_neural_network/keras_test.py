11f1c57de766efc6f12e649fbb5eca8a985b0926
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-11 15:36:10 +0200

    Added x test and train casting to float

--- simple_neural_network/keras_test.py
+++ simple_neural_network/keras_test.py
@@ -13,7 +13,9 @@ from keras.layers import Dense, Dropout
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape(60000, 28*28)
-x_test = x_test.reshape(60000, 28*28)
+x_test = x_test.reshape(10000, 28*28)
+x_test = x_test.astype('float32')
+x_train = x_train.astype('float32')
 
 x_train /= 255
 x_test /= 255
@@ -22,7 +24,7 @@ y_train = keras.utils.to_categorical(y_train, 10) #encoding hot-shot
 y_test = keras.utils.to_categorical(y_train, 10) #encoding hot-shot
 
 model = Sequential()
-model.add(Dense(512, activation="relu", input_shape=(28*28,)))
+model.add(Dense(512, activation="relu", input_shape=(784,)))
 model.add(Dropout(0.2)) #20% szansa na wylaczenie losowego neurona
 model.add(Dense(128, activation="relu")
 model.add(Dropout(0.2))
