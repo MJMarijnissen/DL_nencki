3496727ef6bcec479bab958b21fbdca7c6e9e15c
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-11 15:02:12 +0200

    Corrected Mnist database

--- simple_neural_network/keras_test.py
+++ simple_neural_network/keras_test.py
@@ -6,6 +6,10 @@ Created on Tue Jun 11 14:43:49 2019
 """
 
 import keras 
+from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train.reshape(60000)
