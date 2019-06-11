130557f653cd3722e45274a5940d41dbcb7f1368
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-11 15:42:11 +0200

    Fixed some errors and tested, works

--- simple_neural_network/keras_test.py
+++ simple_neural_network/keras_test.py
@@ -12,27 +12,27 @@ from keras.layers import Dense, Dropout
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 28*28)
-x_test = x_test.reshape(10000, 28*28)
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
 x_test = x_test.astype('float32')
 x_train = x_train.astype('float32')
 
 x_train /= 255
 x_test /= 255
 
-y_train = keras.utils.to_categorical(y_train, 10) #encoding hot-shot
-y_test = keras.utils.to_categorical(y_train, 10) #encoding hot-shot
+y_train = keras.utils.to_categorical(y_train, 10) #encoding One-hot vector
+y_test = keras.utils.to_categorical(y_test, 10) #encoding Obe-hot vector
 
 model = Sequential()
 model.add(Dense(512, activation="relu", input_shape=(784,)))
 model.add(Dropout(0.2)) #20% szansa na wylaczenie losowego neurona
-model.add(Dense(128, activation="relu")
-model.add(Dropout(0.2))
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.2)) #20% szansa na wylaczenie losowego neurona
 model.add(Dense(10, activation='softmax'))
 model.summary()
 
 model.compile(loss = 'categorical_crossentropy', 
-              optimizer='sqd', 
+              optimizer='sgd', 
               metrics=['accuracy']
               )
 
