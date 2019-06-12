0ca40d9ce5b0a019d6c64fa66395a21a7e36406e
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-12 11:25:11 +0200

    Added score and deleted error with reshape

--- simple_neural_network/CNN1.py
+++ simple_neural_network/CNN1.py
@@ -19,8 +19,11 @@ x_train = x_train.astype('float32')
 x_train /= 255
 x_test /= 255
 
-x_train = x_train.reshape(x_train.reshape[0], 28, 28, 1) # 1 bechause grayscale
-x_test = x_test.reshape(x_test.reshape[0], 28, 28, 1) # 1 bechause grayscale
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+y_train = keras.utils.to_categorical(y_train, 10) #encoding One-hot vector
+y_test = keras.utils.to_categorical(y_test, 10) #encoding Obe-hot vector
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(28,28,1))) #padding does not delete the pixels after convolution (retians size of image)
@@ -39,4 +42,5 @@ model.fit(x_train, y_train, batch_size=32, epochs=20, verbose=True, validation_d
 
 score = model.evaluate(x_test, y_test, verbose=False)
 
-print(score)
+print('Test loss', score[0])
+print('Test accuracy', score[1])
