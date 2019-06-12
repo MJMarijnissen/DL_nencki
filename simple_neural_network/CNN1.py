7aa5478ef4c7ec3b4214debaa7b00bd4a5e3df13
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-12 11:32:31 +0200

    improved style

--- simple_neural_network/CNN1.py
+++ simple_neural_network/CNN1.py
@@ -19,7 +19,7 @@ x_train = x_train.astype('float32')
 x_train /= 255
 x_test /= 255
 
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1) # bechause of greyscale
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
 y_train = keras.utils.to_categorical(y_train, 10) #encoding One-hot vector
@@ -36,11 +36,18 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', 
+              optimizer='adadelta', 
+              metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=32, epochs=20, verbose=True, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, 
+          batch_size=32, 
+          epochs=20, 
+          verbose=True, 
+          validation_data=(x_test, y_test))
 
-score = model.evaluate(x_test, y_test, verbose=False)
+score = model.evaluate(x_test, y_test, 
+                       verbose=False)
 
 print('Test loss', score[0])
 print('Test accuracy', score[1])
