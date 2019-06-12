1a4aa31d588851284e63a3caf120b09c72973c70
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-12 10:55:58 +0200

    Deleted reshape

--- simple_neural_network/CNN1.py
+++ simple_neural_network/CNN1.py
@@ -13,10 +13,10 @@ from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
 x_test = x_test.astype('float32')
 x_train = x_train.astype('float32')
 
 x_train /= 255
 x_test /= 255
+
+
