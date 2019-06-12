14f1d9f2f9fbe76e444268145c883b8b6314c143
MJMarijnissen <kubamarijnissen@gmail.com>
2019-06-12 11:12:56 +0200

    Added CNN model

--- simple_neural_network/CNN1.py
+++ simple_neural_network/CNN1.py
@@ -22,4 +22,13 @@ x_test /= 255
 x_train = x_train.reshape(x_train.reshape[0], 28, 28, 1) # 1 bechause grayscale
 x_test = x_test.reshape(x_test.reshape[0], 28, 28, 1) # 1 bechause grayscale
 
-
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(28,28,1))) #padding does not delete the pixels after convolution (retians size of image)
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
